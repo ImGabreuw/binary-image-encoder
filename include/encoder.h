ccd01839b3c10a95af7d7ac17eeda2d0a5fa37da
ImGabreuw <imgabreuw.dev@gmail.com>
2023-11-13 11:27:29 +0000

    refactor: clean code encoder functions

--- include/encoder.h
+++ include/encoder.h
@@ -1,5 +1,10 @@
 #ifndef BINARY_IMAGE_ENCODER_INCLUDE_ENCODER
 
-void encode(Image img, int startRow, int endRow, int startCol, int endCol, char *code);
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool is_uniform(Image image, int start_row, int end_row, int start_col, int end_col);
+
+void encode(Image image, int start_row, int end_row, int start_col, int end_col, char *code);
 
 #endif // BINARY_IMAGE_ENCODER_INCLUDE_ENCODER
--- src/encoder.c
+++ src/encoder.c
@@ -1,52 +1,58 @@
 #include "../include/parser.h"
 #include "../include/encoder.h"
 
-void encode(Image img, int startRow, int endRow, int startCol, int endCol, char *code)
+bool is_uniform(Image image, int start_row, int end_row, int start_col, int end_col)
 {
-  if (startRow > endRow || startCol > endCol)
-  {
-    return; // Caso base: subimagem inválida
-  }
+  bool uniform = true;
+  int first_pixel = image.pixels[start_row * image.width + start_col];
 
-  // Verificar se a imagem é uniforme
-  int isUniform = 1;
-  int firstPixel = img.pixels[startRow * img.width + startCol];
-
-  for (int i = startRow; i <= endRow; ++i)
+  for (int i = start_row; i <= end_row; ++i)
   {
-    for (int j = startCol; j <= endCol; ++j)
+    for (int j = start_col; j <= end_col; ++j)
     {
-      if (img.pixels[i * img.width + j] != firstPixel)
+      if (image.pixels[i * image.width + j] != first_pixel)
       {
-        isUniform = 0;
+        uniform = false;
         break;
       }
     }
-    if (!isUniform)
+
+    if (!uniform)
     {
       break;
     }
   }
 
-  // Se for uniforme, adicionar o código correspondente
-  if (isUniform)
+  return uniform;
+}
+
+void encode(Image image, int start_row, int end_row, int start_col, int end_col, char *code)
+{
+  if (start_row > end_row || start_col > end_col)
   {
-    code = realloc(code, strlen(code) + 2);
-    sprintf(code, "%s%c", code, (firstPixel == 0) ? 'B' : 'P');
+    return; // Caso base: subimagem inválida
+  }
+
+  code = realloc(code, strlen(code) + 2);
+
+  // Se for uniforme, adicionar o código correspondente
+  if (is_uniform(image, start_row, end_row, start_col, end_col))
+  {
+    int first_pixel = image.pixels[start_row * image.width + start_col];
+    sprintf(code, "%s%c", code, (first_pixel == 0) ? 'B' : 'P');
   }
   else
   {
     // Se não for uniforme, adicionar 'X' e dividir a imagem em 4 quadrantes
-    code = realloc(code, strlen(code) + 2);
     sprintf(code, "%sX", code);
 
-    int midRow = (startRow + endRow) / 2;
-    int midCol = (startCol + endCol) / 2;
+    int mid_row = (start_row + end_row) / 2;
+    int mid_col = (start_col + end_col) / 2;
 
     // Chamar a função recursivamente para cada quadrante
-    encode(img, startRow, midRow, startCol, midCol, code);     // 1º quadrante
-    encode(img, startRow, midRow, midCol + 1, endCol, code);   // 2º quadrante
-    encode(img, midRow + 1, endRow, startCol, midCol, code);   // 3º quadrante
-    encode(img, midRow + 1, endRow, midCol + 1, endCol, code); // 4º quadrante
+    encode(image, start_row, mid_row, start_col, mid_col, code);     // 1º quadrante
+    encode(image, start_row, mid_row, mid_col + 1, end_col, code);   // 2º quadrante
+    encode(image, mid_row + 1, end_row, start_col, mid_col, code);   // 3º quadrante
+    encode(image, mid_row + 1, end_row, mid_col + 1, end_col, code); // 4º quadrante
   }
 }
